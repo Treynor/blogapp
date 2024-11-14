e53f7979aeca25975cb0f8e3f0b150b0e0ffba47
Treynor <treyno@gmail.com>
2024-11-14 14:27:53 +0000

    toggling

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,12 +1,21 @@
+"use client"
+
 import styles from "./themeToggle.module.css"
-import React from 'react'
+import React, { useContext } from 'react'
 import Image from "next/image"
+import { ThemeContext } from "@/app/context/ThemeContext"
 
 const ThemeToggle = () => {
+
+
+  const {toggle, theme} = useContext(ThemeContext)
+
+  console.log(theme)
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <Image src="/moon.png" alt="" width={14} height={14}/>
-      <div className={styles.ball}></div>
+      <div className={styles.ball} style={theme === 'dark' ? {background:'#white'} : {right:1, background:"#0f172a"}}></div>
       <Image src="/sun.png" alt="" width={14} height={14}/>
       
     </div>
