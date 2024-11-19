678544ac4372dbc1f3363739d048683e64ca364f
Treynor <treyno@gmail.com>
2024-11-19 16:49:56 +0000

    more2

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from "./Menu.module.css"
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Menu = () => {
   return (
-    <div className={styles.container}>Menu</div>
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What's hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+            <h3 classname={styles.postTitle}>Es un hecho establecido hace demasiado tiempo que un lector</h3>
+            <div className={styles.details}>
+              <span className={styles.username}>Tony Reynolds</span>
+              <span className={styles.date}>19.11.24</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -5,18 +5,20 @@ import Link from 'next/link'
 const Card = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.imageContainer}></div>
-            <Image src='/p1.jpeg' alt="" fill/>
-            <div className={styles.textContainer}></div>
+            <div className={styles.imageContainer}>
+                <Image src='/p1.jpeg' alt="" fill className={styles.image}/>
+            </div>
+            <div className={styles.textContainer}>
                 <div className={styles.detail}>
-                    <span className={styles.date}>18.11.24</span>    
+                    <span className={styles.date}>18.11.24 - </span>    
                     <span className={styles.category}>CULTURE</span>
                 </div>
                 <Link href='/'>
                     <h1>Lorem isum dolor sit amet consectetur adipsiscing elit.</h1>    
                 </Link>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat suscipit dapibus. Pellentesque fringilla cursus convallis. Integer elementum facilisis maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
-                <Link href='/'>Read More</Link>
+                <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consequat suscipit dapibus. Pellentesque fringilla cursus convallis. Integer elementum facilisis maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                <Link href='/' className={styles.link}>Read More</Link>
+            </div>
         </div>
     )
 }
