629fd1dfdd58dd3790d2cda057cd89c6e8d0985f
Treynor <treyno@gmail.com>
2024-11-20 16:11:08 +0000

    more

--- src/app/components/Menu/Menu.jsx
+++ src/app/components/Menu/Menu.jsx
@@ -15,10 +15,53 @@ const Menu = () => {
           </div>
           <div className={styles.textContainer}>
             <span className={`${styles.category} ${styles.travel}`}>Travel</span>
-            <h3 classname={styles.postTitle}>Es un hecho establecido hace demasiado tiempo que un lector</h3>
-            <div className={styles.details}>
+            
+            <div className={styles.detail}>
+            <h3 classname={styles.postTitle}>Lorem ipsum color sit amet consectetur adispcig</h3>
               <span className={styles.username}>Tony Reynolds</span>
-              <span className={styles.date}>19.11.24</span>
+              <span className={styles.date}> - 19.11.24</span>
+            </div>
+          </div>
+        </Link>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>Culture</span>
+            
+            <div className={styles.detail}>
+            <h3 classname={styles.postTitle}>Lorem ipsum color sit amet consectetur adispcig</h3>
+              <span className={styles.username}>Tony Reynolds</span>
+              <span className={styles.date}> - 19.11.24</span>
+            </div>
+          </div>
+        </Link>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            
+            <div className={styles.detail}>
+            <h3 classname={styles.postTitle}>Lorem ipsum color sit amet consectetur adispcig</h3>
+              <span className={styles.username}>Tony Reynolds</span>
+              <span className={styles.date}> - 19.11.24</span>
+            </div>
+          </div>
+        </Link>
+        <Link href='/' className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src='/p1.jpeg' alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>Fashion</span>
+            
+            <div className={styles.detail}>
+            <h3 classname={styles.postTitle}>Lorem ipsum color sit amet consectetur adispcig</h3>
+              <span className={styles.username}>Tony Reynolds</span>
+              <span className={styles.date}> - 19.11.24</span>
             </div>
           </div>
         </Link>
